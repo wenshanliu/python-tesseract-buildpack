1d5e328265c84626312261f565c5a29f227c7e15
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-06-22 11:07:18 -0400

    Removing searching for Downloaded to fix the flask tests

--- cf_spec/integration/deploy_a_python_app_spec.rb
+++ cf_spec/integration/deploy_a_python_app_spec.rb
@@ -53,7 +53,6 @@ HERE
 
             browser.visit_path('/')
             expect(browser).to have_body('Hello, World!')
-            expect(app).to have_logged(/Downloaded \[file:\/\/.*\]/)
 
             expect(app).not_to have_internet_traffic
 
@@ -101,7 +100,7 @@ HERE
 
             browser.visit_path('/')
             expect(browser).to have_body('Hello, World!')
-            expect(app).to have_logged(/Downloaded \[file:\/\/.*\]/)
+
           end
         end
         context 'deploy a django web app' do
